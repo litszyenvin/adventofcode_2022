237fc99ecaf0d912ab76212fcc122cc9c42b01e7
Vincent Li <vincent.li@ocado.com>
2022-12-05 22:16:36 +0000

    day 5 part 2 solved

--- day5.py
+++ day5.py
@@ -4,13 +4,15 @@ from math import floor
 import re
 
 np.set_printoptions(threshold=10000000000)
+NO_OF_COLUMNS = 9
+INITIAL_HEIGHT = 7
 
 with open('data/day5_input.txt') as f:
     rows = f.readlines()
 stack = np.empty((9,100), dtype=str)
 stack_list = []
 # column_index = 0
-row_index = 7
+row_index = INITIAL_HEIGHT
 for x, row in enumerate(rows):
     if x <= 8:
         column_indexes = [m.start() for m in re.finditer('[A-Z]', row)]
@@ -21,7 +23,7 @@ for x, row in enumerate(rows):
         
         row_index -= 1
 
-for i in range(0,9):
+for i in range(0,NO_OF_COLUMNS):
     stack_list.append(stack[i][stack[i]!=''].tolist())
 # print(stack_list)
 
@@ -34,11 +36,11 @@ for x, row in enumerate(rows):
         origin = int(instruction[1])
         destination = int(instruction[2])
         temp = stack_list[origin-1][-height:]
-        temp = temp[::-1]
+        # temp = temp[::-1]
         stack_list[origin-1] = stack_list[origin-1][:len(stack_list[origin-1])-height]
         stack_list[destination-1] = stack_list[destination-1] + temp
 
 for column in stack_list:
-    print(column[-1])
+    print(column[-1], end='')
 
 
