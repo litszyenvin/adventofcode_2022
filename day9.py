203915211fffb6192b55357c3dd609b7f1cff9f5
Vincent Li <litszyenvin@gmail.com>
2022-12-10 10:40:08 +0000

    day9 part2 fu

--- day9.py
+++ day9.py
@@ -6,22 +6,26 @@ with open('data\day9_input.txt') as f:
     lines = f.readlines()
 lines = [line.strip('\n') for line in lines]
 
-head_path = ['0,0']
-tail_path = ['0,0']
-head_x_cor = head_y_cor = tail_x_cor = tail_y_cor = 0
-last_head_x_cor = last_head_y_cor = 0
+string_path = ['0,0']
+string_path *= 10
+
+x_cor = 0
+y_cor = 0
+last_x_cor = 0
+last_y_cor = 0
+
 
 # fig, ax = plt.subplots(figsize=(5, 5))
 for line in lines:
     for _ in range(int(line.split()[1])):
         if line.split()[0] == 'L':
-            head_x_cor -= 1
+            x_cor -= 1
         if line.split()[0] == 'R':
-            head_x_cor += 1
+            x_cor += 1
         if line.split()[0] == 'U':
-            head_y_cor += 1
+            y_cor += 1
         if line.split()[0] == 'D':
-            head_y_cor -= 1   
+            y_cor -= 1   
     
         dist = ((head_x_cor-tail_x_cor)**2 + (head_y_cor-tail_y_cor)**2)**0.5
         if dist >= 2:
@@ -32,7 +36,7 @@ for line in lines:
         last_head_y_cor = head_y_cor
         head_path.append(str(head_x_cor) + ',' + str(head_y_cor))
         tail_path.append(str(tail_x_cor) + ',' + str(tail_y_cor))
-        print(str(head_x_cor)+','+str(head_y_cor)+'    '+str(tail_x_cor)+','+str(tail_y_cor))
+        # print(str(head_x_cor)+','+str(head_y_cor)+'    '+str(tail_x_cor)+','+str(tail_y_cor))
         # plt.xlim([-80, 30])
         # plt.ylim([-80, 30])        
         # plt.plot(head_x_cor,head_y_cor, 'o')
